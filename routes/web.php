ffa129a9d43e7a649b747759317f34f60931cf86
Abdullah Hamdy <abdullah.hamdi.1998@gmail.com>
2023-01-06 19:22:21 +0200

    added posts model

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('home');
+    });
+});
