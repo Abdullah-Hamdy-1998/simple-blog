b804b43c930d019f04c41909bf8c8423f4654ea4
Abdullah Hamdy <abdullah.hamdi.1998@gmail.com>
2023-01-06 20:15:59 +0200

    Add "Add Post" view

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,10 +5,23 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">asdfsdafds</div>
-
+                    <div class="card-header">Add Post</div>
                     <div class="card-body">
+                        <form action="{{ route('posts.store') }}" method="POST">
+                            @csrf
 
+                            <div class="form-group mt-3">
+                                <label for="title">Title</label>
+                                <input type="text" class="form-control" id="title" name="title" required>
+                            </div>
+
+                            <div class="form-group mt-3">
+                                <label for="body">Body</label>
+                                <textarea class="form-control" id="body" name="body" rows="3" required></textarea>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary mt-3">Add Post</button>
+                        </form>
                     </div>
                 </div>
             </div>
